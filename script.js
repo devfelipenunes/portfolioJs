21264ae972463b1da3804d909cbfcab55e13188a
devfelipenunes <devfelipenunes@gmail.com>
2022-05-01 12:03:01 -0300

    Atualização menu

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
+  constructor(mobileMenu, navList, navLinks) {
       this.mobileMenu = document.querySelector(mobileMenu);
       this.navList = document.querySelector(navList);
       this.navLinks = document.querySelectorAll(navLinks);
       this.activeClass = "active";
-  
+      
       this.handleClick = this.handleClick.bind(this);
     }
   
     animateLinks() {
       this.navLinks.forEach((link, index) => {
         link.style.animation
-          ? (link.style.animation = "")
+        ? (link.style.animation = "")
           : (link.style.animation = `navLinkFade 0.5s ease forwards ${
               index / 7 + 0.3
             }s`);
-      });
-    }
+          });
+        }
   
     handleClick() {
       this.navList.classList.toggle(this.activeClass);
       this.mobileMenu.classList.toggle(this.activeClass);
       this.animateLinks();
     }
-  
+    
     addClickEvent() {
       this.mobileMenu.addEventListener("click", this.handleClick);
     }
@@ -40,5 +40,16 @@ class MobileNavbar {
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
-  );
-  mobileNavbar.init();
+    );
+    mobileNavbar.init();
+    
+/*
+MENU
+*/
+let menuToggle = document.querySelector('.menuToggle');
+let navigation = document.querySelector('.navigation');
+menuToggle.onclick = function(){
+  navigation.classList.toggle('active')
+}
+
+
